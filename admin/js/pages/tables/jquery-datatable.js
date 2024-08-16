93f97d5f507f740d1c8260b8314492ce86dc6d21
russ <russelcuevas0@gmail.com>
2024-08-16 11:57:40 +0800

    frontend

--- admin/js/pages/tables/jquery-datatable.js
+++ admin/js/pages/tables/jquery-datatable.js
@@ -3,12 +3,16 @@ $(function () {
         responsive: true
     });
 
-    //Exportable table
+    // Exportable table
     $('.js-exportable').DataTable({
         dom: 'Bfrtip',
         responsive: true,
         buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
+            { extend: 'copy', className: 'custom-button' },
+            { extend: 'csv', className: 'custom-button' },
+            { extend: 'excel', className: 'custom-button' },
+            { extend: 'pdf', className: 'custom-button' },
+            { extend: 'print', className: 'custom-button' }
         ]
     });
 });
